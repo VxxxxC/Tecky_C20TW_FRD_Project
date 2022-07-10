043270177deb848d6707b2f83233e288d30f7596
Veper(Mac) <veper.chesley@protonmail.com>
2022-07-10 17:02:27 +0800

    styling for sideMenu

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,6 @@
 import React from "react";
 import logo from "./logo.svg";
-import {
-  BrowserRouter,
-  Routes,
-  Route,
-  Link
-} from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import "./App.css";
 import Head from "./page/head/head";
 import Item from "./page/elements/item";
@@ -13,18 +8,18 @@ import Footer from "./page/footer/footer";
 import Main from "./page/main/main";
 
 function App() {
-  const theme = ["cupcake", "lofi"]
+  const theme = ["cupcake", "lofi"];
   return (
     <html data-theme={theme[1]}>
       <BrowserRouter>
-      <Head/>
-      <div className="m-5">
-        <Routes>
-          <Route path="/" element={ <Main/>}/> 
-        </Routes>
-      </div>
-        <Footer/>
-     </BrowserRouter>
+          <Head />
+          <div className="m-5">
+            <Routes>
+              <Route path="/" element={<Main />} />
+            </Routes>
+          </div>
+          <Footer />
+      </BrowserRouter>
     </html>
   );
 }
