096e11b744c38911d350b9a5712473e3205cca5e
Veper(Mac) <veper.chesley@protonmail.com>
2022-07-15 12:54:05 +0800

    backing up login auth progress

--- server/signUpRoute.ts
+++ server/signUpRoute.ts
@@ -7,47 +7,80 @@ import { knex } from './knex'; /* MUST BE very careful !!! don't import "knex" ,
 export let userRoute = express.Router();
 
 
-
-// userRoute.get('/signup', (req, res) => {
-//    console.log("Hello")
-//    res.json("Hello World")
-// })
-
+/******************* Signup Form **********************/
 userRoute.post('/signup', async (req, res) => {
    console.log(req.body);
    const { email, password } = req.body;
-   console.log({ email, password });
-
-   let userExist: any = await knex('users').where({ email: email })
-   if (!userExist) {
-      console.log("Good! This email not exist yet!")
-   } else {
-      res.status(409).end('email duplicated')
-   }
+   console.log({
+      "Signup email : ": email,
+      "Signup password : ": password
+   });
 
    const passwordHash = await bcrypt.hash(password, 12);
    console.log(passwordHash)
 
-   const result: any = await knex('users').insert({ email: email, password: passwordHash, created_at: new Date() })
-   console.log(result)
+   const comparePassword = await bcrypt.compare(password, passwordHash);
+   console.log(comparePassword)
 
-   const { insertId } = result;
-   console.log({ insertId })
+   // let userExist: any = await knex('users').select("*").where({ email: email }).first().returning('id')
+   // if (!userExist || undefined) {
+   //    console.log({ userExist })
+   //    console.log("Good! This email not exist yet!")
+   //    return res.status(200).end('email not exist');
+   // } else {
+   //    console.log({ userExist })
+   //    console.log("email duplicated, server returned")
+   //    return res.status(409).end('email duplicated')
+   // }
 
-   jwt.sign({
-      id: insertId,
-   },
-      env.JWT_SECRET,
-      {
-         expiresIn: '2d',
-      },
-      (err, token) => {
-         if (err) {
-            return res.status(500).send(err);
-         } else {
-            res.status(200).send({ token });
-         }
-      }
-   )
+   // const result: any = await knex('users').insert({ email: email, password: passwordHash, created_at: new Date() })
+   // console.log(result)
+
+   // const { insertId } = result;
+   // console.log({ insertId })
+
+   // /* for after login */
+   // jwt.sign({
+   //    id: insertId,
+   // },
+   //    env.JWT_SECRET,
+   //    {
+   //       expiresIn: '2d',
+   //    },
+   //    (err, token) => {
+   //       if (err) {
+   //          return res.status(500).send(err);
+   //       } else {
+   //          res.status(200).send({ token });
+   //       }
+   //    }
+   // )
 })
 
+
+/*************  Check User email and hashed password ******************/
+userRoute.get('/', async (req, res) => {
+   console.log(req.body)
+   const { email, password } = req.body;
+   console.log({
+      "Login email : ": email,
+      "Login password : ": /*checkPassword()*/ password
+   });
+
+   //let verifyUser = await knex('users').where({ email: email, password: password}).first().returning('id')
+   let verifyUser = await knex('users').select("*").where("email", email).first();
+   console.log({ verifyUser })
+
+   if (verifyUser) {
+      const comparePassword = await bcrypt.compare(password, verifyUser.password);
+      // console.log(comparePassword)
+      if (comparePassword) {
+         return res.json({ success: true, token: "" })
+      } else {
+         return res.json({ success: false, msg: "Wrong Password/User doesn't existed" })
+      }
+   } else {
+      return res.json({ success: false, msg: "Wrong Password/User doesn't existed" })
+   }
+   //res.end("User checked")
+})
