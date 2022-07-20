e8243c79eacc690e4a932485ce66255fe0429261
Veper(Mac) <veper.chesley@protonmail.com>
2022-07-20 22:55:24 +0800

    create product page done , button function , server routing and DB fetching still in progress..

--- app/src/page/userProfile/createProduct.tsx
+++ app/src/page/userProfile/createProduct.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import useStorageState from "react-use-storage-state";
 
 function CreateProduct() {
@@ -9,9 +9,137 @@ function CreateProduct() {
   }
 
   const bios: any = useStorageState("createBios", "");
-  console.log(bios);
+  console.log(bios[0]);
 
-  return <button onClick={changeBios}>Go Back</button>;
+  const [value, setValue] = useState("");
+
+  const handleChange = (event: { target: { value: string } }) => {
+    const result = event.target.value.replace(/\D/g, "");
+
+    setValue(result);
+  };
+
+  console.log(value);
+  console.log(typeof value);
+  console.log(Number(value));
+
+  return (
+    <>
+      <button
+        onClick={changeBios}
+        className="m-10 btn-primary w-[100px] h-[60px] rounded-lg"
+      >
+        <svg
+          className="w-24 h-6"
+          fill="none"
+          stroke="currentColor"
+          viewBox="0 0 24 24"
+          xmlns="http://www.w3.org/2000/svg"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            stroke-width="3"
+            d="M7 16l-4-4m0 0l4-4m-4 4h18"
+          ></path>
+        </svg>
+        Go Back
+      </button>
+
+      <div className="mx-10">
+        <div className="bg-[#00000022] h-[2px] mb-[50px]"></div>
+        <div className="w-[90vw] border-[black] border-2 bg-[#0000001d] p-10">
+          {/* create product from start from here! */}
+          <form>
+            <div className="w-[17rem] text-[white] text-3xl bg-[black]">
+              Create New Product
+            </div>
+
+            <div className="grid overflow- md grid-cols-3 auto-rows-auto gap-2 grid-flow-row ">
+              <div className="m-3 text-2xl font-bold">Upload file</div>
+              <div className="col-start-1 col-end-4 h-[10rem] border-[#F96248] border-2 border-dashed rounded-3xl flex justify-center items-center">
+                <div className="w-[15rem] h-[3rem] border-2 bg-[#0000009a] text-[white] rounded-full flex justify-center items-center">
+                  Choose Your Product Pic! :)
+                </div>
+              </div>
+              <div className="m-3 text-2xl font-bold">Price</div>
+              <input
+                type="text"
+                placeholder="Enter Price for one piece"
+                value={value}
+                onChange={handleChange}
+                className="col-start-1 col-end-4 h-[3rem] p-5 hover:border-[#3EC8F9] hover:border-2 rounded-3xl flex justify-center items-center"
+              />
+              <div className="m-3 text-2xl font-bold">Product Name</div>
+
+              <input
+                type="text"
+                placeholder="Enter Name for your piece :)"
+                className="col-start-1 col-end-4 h-[3rem] p-5 hover:border-[#3EC8F9] hover:border-2 rounded-3xl flex justify-center items-center"
+              />
+              <div className="m-3 text-2xl font-bold flex items-center">
+                Description
+                <span className="text-[grey] text-xl font-normal">
+                  (Optional)
+                </span>
+              </div>
+              <input
+                type="text"
+                placeholder="Tell us the story of piece :)"
+                className="col-start-1 col-end-4 h-[3rem] p-5 hover:border-[#3EC8F9] hover:border-2 rounded-3xl flex justify-center items-center"
+              />
+              <div className="m-3 text-2xl font-bold flex items-center">
+                Properties
+                <span className="text-[grey] text-xl font-normal">
+                  (Optional)
+                </span>
+              </div>
+              <input
+                type="text"
+                placeholder="e.g. Size"
+                className="col-start-1 col-end-2 h-[3rem] p-5 hover:border-[#3EC8F9] hover:border-2 rounded-3xl flex justify-center items-center"
+              />
+              <input
+                type="text"
+                placeholder="e.g. Large"
+                className="col-start-2 col-end-3 h-[3rem] p-5 hover:border-[#3EC8F9] hover:border-2 rounded-3xl flex justify-center items-center"
+              />
+
+              <div className="col-start-1 col-end-1 m-3 text-2xl font-bold flex items-center">
+                Materials
+                <span className="text-[grey] text-xl font-normal">
+                  (Optional)
+                </span>
+              </div>
+              <input
+                type="text"
+                placeholder="e.g. Leather"
+                className="col-start-1 col-end-4 h-[3rem] p-5 hover:border-[#3EC8F9] hover:border-2 rounded-3xl flex justify-center items-center"
+              />
+              <div className="col-start-1 col-end-1 m-3 w-[30rem] text-2xl font-bold flex items-center">
+                Alternative text for piece
+                <span className="text-[grey] text-xl font-normal">
+                  (Optional)
+                </span>
+              </div>
+              <input
+                type="text"
+                placeholder="e.g. Leave some message to attach on your piece <3"
+                className="col-start-1 col-end-4 h-[3rem] p-5 hover:border-[#3EC8F9] hover:border-2 rounded-3xl flex justify-center items-center"
+              />
+
+              <button
+                type="submit"
+                className="col-start-1 mt-3 h-[3rem] p-5 hover:bg-[white] border-[white] bg-[#3EC8F9] hover:border-[#3EC8F9] text-[white] hover:text-[#3EC8F9] font-bold border-4 rounded-3xl flex justify-center items-center"
+              >
+                <p>Create Product!</p>
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default CreateProduct;
--- app/src/page/userProfile/user.tsx
+++ app/src/page/userProfile/user.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link, Routes } from "react-router-dom";
 import useWindowDimensions from "../../hook/useWindowDimensions";
 import CreateProduct from "./createProduct";
@@ -10,13 +10,12 @@ function User() {
   const { height, width } = useWindowDimensions();
 
   const [click, setClick] = useStorageState("createBios", "");
+  const bios: any = useStorageState("createBios", "");
+  console.log(bios[0]);
 
   function changeBios() {
     setClick("true");
   }
-
-  const bios: any = useStorageState("createBios", "");
-  console.log(bios);
 
   const userAddress = "0x12bd534961a86dcf660dd3f3745ad6d4045eb77d";
 
@@ -41,60 +40,62 @@ function User() {
       </div>
 
       <div className="block justify-center">
-        {/************** Top container *****************/}
-        <div className={!responsive ? webContainer : mobileContainer}>
-          {/************** Top left container *****************/}
-          <div className="m-5 h-[350px] w-[400px]  mobile:-translate-y-[30%] desktop:-translate-y-[30%] rounded-2xl flex flex-col">
-            <div className="m-5 space-y-5">
-              <div className="rounded-full border-8 border-[white] w-[150px] h-[150px] flex justify-center items-center bg-[#80808044]">
-                {/* User picture */}
-                <img
-                  className="object-fill rounded-full"
-                  src="https://img.rarible.com/prod/image/upload/t_avatar_big/prod-users/0x12bd534961a86dcf660dd3f3745ad6d4045eb77d/avatar/QmNnnLGuiUKgg6hhCtwkirKQFPG5ni4pgYW11CRhVuUsCV"
-                />
-              </div>
-              <div className="text-[black] text-4xl">
-                <p>Ali Hadian</p>
-              </div>
-              <div className="flex flex-row gap-x-10">
-                <button className={messageBtn}>MESSAGE</button>
-                <button className={messageBtn}>FOLLOW</button>
-              </div>
-              <div className="text-xl font-sans">TRAVEL YOUR IMAGINATION</div>
-            </div>
-          </div>
-          {/************** Top right container *****************/}
-          <div className=" h-[400px] w-[400px rounded-2xl flex flex-col text-sm">
-            <div className="m-5 rounded-3xl border-[#01010139] border-2 w-[300px] h-[250px] p-5 flex flex-col justify-between">
-              <div className="flex flex-row justify-between">
-                <p>FOLLOWER</p>
-                <p>100</p>
-              </div>
-              <div className="flex flex-row justify-between">
-                <p>FOLLOWING</p>
-                <p>300</p>
-              </div>
-              <div className="flex flex-row justify-between">
-                <div>Address</div>
-                <a
-                  href="https://etherscan.io/"
-                  target="popup"
-                  className="w-[15vw] truncate"
-                >
-                  {userAddress}
-                </a>
+        <>
+          {/************** Top container *****************/}
+          <div className={!responsive ? webContainer : mobileContainer}>
+            {/************** Top left container *****************/}
+            <div className="m-5 h-[350px] w-[400px]  mobile:-translate-y-[30%] desktop:-translate-y-[30%] rounded-2xl flex flex-col">
+              <div className="m-5 space-y-5">
+                <div className="rounded-full border-8 border-[white] w-[150px] h-[150px] flex justify-center items-center bg-[#80808044]">
+                  {/* User picture */}
+                  <img
+                    className="object-fill rounded-full"
+                    src="https://img.rarible.com/prod/image/upload/t_avatar_big/prod-users/0x12bd534961a86dcf660dd3f3745ad6d4045eb77d/avatar/QmNnnLGuiUKgg6hhCtwkirKQFPG5ni4pgYW11CRhVuUsCV"
+                  />
+                </div>
+                <div className="text-[black] text-4xl">
+                  <p>Ali Hadian</p>
+                </div>
+                <div className="flex flex-row gap-x-10">
+                  <button className={messageBtn}>MESSAGE</button>
+                  <button className={messageBtn}>FOLLOW</button>
+                </div>
+                <div className="text-xl font-sans">TRAVEL YOUR IMAGINATION</div>
               </div>
             </div>
-            <button
-              onClick={changeBios}
-              className="m-5 h-[100px] w-[300px] border-[#F96248] border-2 text-[white] text-3xl bg-[#f96248b4] rounded-lg flex justify-center items-center transition ease-linear duration-150 hover:scale-110 hover:bg-[#F96248]"
-            >
-              <p>Create Product</p>
-            </button>
+            {/************** Top right container *****************/}
+            <div className=" h-[400px] w-[400px rounded-2xl flex flex-col text-sm">
+              <div className="m-5 rounded-3xl border-[#01010139] border-2 w-[300px] h-[250px] p-5 flex flex-col justify-between">
+                <div className="flex flex-row justify-between">
+                  <p>FOLLOWER</p>
+                  <p>100</p>
+                </div>
+                <div className="flex flex-row justify-between">
+                  <p>FOLLOWING</p>
+                  <p>300</p>
+                </div>
+                <div className="flex flex-row justify-between">
+                  <div>Address</div>
+                  <a
+                    href="https://etherscan.io/"
+                    target="popup"
+                    className="w-[15vw] truncate"
+                  >
+                    {userAddress}
+                  </a>
+                </div>
+              </div>
+              <button
+                onClick={changeBios}
+                className="m-5 h-[100px] w-[300px] border-[#F96248] border-2 text-[white] text-3xl bg-[#f96248b4] rounded-lg flex justify-center items-center transition ease-linear duration-150 hover:scale-110 hover:bg-[#F96248]"
+              >
+                <p>Create Product</p>
+              </button>
+            </div>
           </div>
-        </div>
-        {/* Product Section */}
-        {bios.createBios == "true" ? <CreateProduct /> : <ProductSection />}
+          {/* Product Section */}
+          {bios[0] == "true" ? <CreateProduct /> : <ProductSection />}
+        </>
       </div>
     </>
   );
