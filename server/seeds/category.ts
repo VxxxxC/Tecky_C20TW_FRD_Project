d38a6c25c878b4dff5e6e66232be64f4d1d0d3ff
Paul Kwong <paulkwong622@gmail.com>
2022-07-21 16:07:50 +0800

    seed for product category

---
+++ server/seeds/category.ts
@@ -0,0 +1,17 @@
+import { Knex } from "knex";
+import cateogrySeed from '../stuff/category.json';
+
+export async function seed(knex: Knex): Promise<void> {
+
+    const timestamp_primary = "2022-02-02"
+
+    // Deletes ALL existing entries
+    await knex("category").del();
+
+    // Insert Category
+    for (let each of cateogrySeed){
+        await knex("category").insert([
+            {name: each.name, updated_at: timestamp_primary, created_at: timestamp_primary},
+        ]);
+    }
+};
--- server/seeds/product_type.ts
+++ server/seeds/product_type.ts
@@ -2,13 +2,13 @@ import { Knex } from "knex";
 
 export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
-    const timestamp = "2018-01-01"
-
     await knex("product_type").del();
-
-    // Inserts seed entries
+    
+    // Inserts product_type
+    const timestamp_primary = "2022-02-02"
     await knex("product_type").insert([
-        { updated_at: timestamp, created_at: timestamp },
-        { updated_at: timestamp, created_at: timestamp },
+        { updated_at: timestamp_primary, created_at: timestamp_primary },
+        { updated_at: timestamp_primary, created_at: timestamp_primary },
     ]);
+
 };
